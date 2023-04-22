ba1aa65792d03502e1dca9ec12cef11713d4f9bf
Aidanhubley <ah1632@messiah.edu>
2023-04-22 01:41:31 -0400

    Started Second Screen + Navigation

--- App.tsx
+++ App.tsx
@@ -1,7 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { Button, Image, Pressable, StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 
-export default function App() {
+
+// Welcome Screen
+function Welcome({navigation}: {navigation: any}) {
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -9,12 +14,51 @@ export default function App() {
       </View>
       <Image style={styles.logo} source={require('./assets/logo/Dark_Logo.png')} />
       <View style={styles.footer}>
-        <Pressable style={styles.footer_pressable} onPress={() =>alert('Continue')}>
+        <Pressable style={styles.footer_pressable} onPress={() => navigation.navigate('Content')}>
           <Text style={styles.footer_text}>Continue</Text>
         </Pressable>
       </View>
       <StatusBar style="auto" />
     </View>
+  );
+}
+
+// Main Content Screen
+function Content({navigation}: {navigation: any}) {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.header_text}>Content</Text>
+      </View>
+      <View style={styles.footer}>
+        <Pressable style={styles.footer_pressable} onPress={() => navigation.goBack()}>
+          <Text style={styles.footer_text}>Back</Text>
+        </Pressable>
+      </View>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+
+// Navigation Stack 
+const Stack = createNativeStackNavigator();
+
+function MyStack() {
+  return (
+    <Stack.Navigator initialRouteName="Welcome">
+      <Stack.Screen name="Welcome" component={Welcome} options={{headerShown: false}}/>
+      <Stack.Screen name="Content" component={Content} options={{headerShown: false}}/>
+    </Stack.Navigator>
+  );
+}
+
+// App Call
+export default function App() {
+  return (
+    <NavigationContainer>
+      <MyStack />
+    </NavigationContainer>
   );
 }
 
