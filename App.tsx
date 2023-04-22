652f2018c755919d69c8fdf65dad9497b19a6ee0
Aidanhubley <ah1632@messiah.edu>
2023-04-21 21:37:00 -0400

    First Screen!

--- App.tsx
+++ App.tsx
@@ -1,12 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, Pressable, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={{color: "#fff"}}>Open up App.tsx to start working on your app!!</Text>
+      <View style={styles.header}>
+        <Text style={styles.header_text}>Welcome to Prayer Circle</Text>
+      </View>
+      <Image style={styles.logo} source={require('./assets/logo/Dark_Logo.png')} />
+      <View style={styles.footer}>
+        <Pressable style={styles.footer_pressable} onPress={() =>alert('Continue')}>
+          <Text style={styles.footer_text}>Continue</Text>
+        </Pressable>
+      </View>
       <StatusBar style="auto" />
-      <Text>Test</Text>
     </View>
   );
 }
@@ -16,6 +23,41 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#2F3E69',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-between',
   },
+  header: {
+    paddingTop: 60,
+  },
+  header_text: {
+    color: '#F7F1E3',
+    textAlign: 'center',
+    fontSize: 50, 
+    fontWeight: 'bold',
+    padding: 10
+  },
+  logo: {
+    bottom: 80,
+    height: 275,
+    width: 263
+  },
+  footer: {
+    backgroundColor: "#2B2B2B",
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    padding: 13,
+    width: 415,
+    alignItems: 'center'
+  },
+  footer_pressable: {
+    backgroundColor: "#006E5B",
+    borderRadius: 30,
+    width: 390,
+    alignItems: 'center'
+  },
+  footer_text: {
+    color: "#F7F1E3", 
+    fontSize: 50, 
+    fontWeight: 'bold',
+    padding: 10
+  }
 });
